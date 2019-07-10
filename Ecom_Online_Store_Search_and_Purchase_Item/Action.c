a30fa67dcff932f256d54d8a9e099766280dd975
weisun10 <weisun10@users.noreply.github.com>
2019-07-10 22:50:40 +0800

    add Ecom_Online_Store

---
+++ Ecom_Online_Store_Search_and_Purchase_Item/Action.c
@@ -0,0 +1,29 @@
+Action()
+{
+	
+//comment 1	
+	lr_start_transaction("Search_Item");
+
+	web_url("52.39.153.71", 
+		"URL=http://52.39.153.71/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("Search_Item", LR_AUTO);
+	
+	lr_start_transaction("Purchase_Item");
+	web_url("secondPage.html", 
+		"URL=http://52.39.153.71/secondPage.html", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://52.39.153.71/", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		LAST);
+lr_end_transaction("Purchase_Item", LR_AUTO);
+	return 0;
+}
