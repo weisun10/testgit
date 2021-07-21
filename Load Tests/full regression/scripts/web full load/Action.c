c2892ebb096879b991145ca89e940d4a4e4a7231
Wei Sun <weisun10@users.noreply.github.com>
2021-07-21 23:32:10 +0800

    Update script

--- Load Tests/full regression/scripts/web full load/Action.c
+++ Load Tests/full regression/scripts/web full load/Action.c
@@ -7,14 +7,14 @@ Action()
 	lr_end_transaction("trans1", LR_PASS);	
 	
 	lr_start_transaction("trans2");	
-	lr_think_time(2);
+	lr_think_time(5);
 	lr_user_data_point("HTTP_200", 1);		
-	lr_error_message("my error message test");	
+	lr_error_message("my error message test demo 123");	
 	lr_end_transaction("trans2", LR_FAIL);		
 
 	lr_start_transaction("trans test");	
 	lr_think_time(1);
-    lr_user_data_point("mic_recv", 30);	
+        lr_user_data_point("mic_recv", 30);	
 	lr_end_transaction("trans test", LR_FAIL);		
 	
 	return 0;
