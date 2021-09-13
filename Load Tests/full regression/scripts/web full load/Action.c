8847441724247b798148fa52b0abab020885f350
Wei Sun <wei.sun10@microfocus.com>
2021-09-13 14:20:58 +0800

    update script and test settings

--- Load Tests/full regression/scripts/web full load/Action.c
+++ Load Tests/full regression/scripts/web full load/Action.c
@@ -9,13 +9,13 @@ Action()
 	lr_start_transaction("trans2");	
 	lr_think_time(5);
 	lr_user_data_point("HTTP_200", 1);		
-	lr_error_message("my error message test demo 12346");	
+	lr_error_message("my error message test demo");	
 	lr_end_transaction("trans2", LR_FAIL);		
 
 	lr_start_transaction("trans test");	
 	//think time
 	lr_think_time(1);
-	lr_user_data_point("mic_recv", 30);	
+	lr_user_data_point("mic_recv", 100);	
 	lr_end_transaction("trans test", LR_FAIL);		
 	
 	return 0;
