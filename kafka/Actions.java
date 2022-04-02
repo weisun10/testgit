c859c0225e98927d1e552afd97d581f861bfa083
Wei Sun <wei.sun10@microfocus.com>
2022-04-02 10:20:15 +0800

    add kafka script

---
+++ kafka/Actions.java
@@ -0,0 +1,34 @@
+/*
+ * LoadRunner Kafka script. (Build: _build_number_)
+ * 
+ * Script Description: 
+ *                     
+ */
+
+import lrapi.lr;
+import mf.protocols.LrKafka.*;
+
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.clients.producer.Callback;
+
+public class Actions
+{
+
+	public int init() throws Throwable {
+		return 0;
+	}//end of init
+
+
+	public int action() throws Throwable {
+		return 0;
+	}//end of action
+
+
+	public int end() throws Throwable {
+		return 0;
+	}//end of end
+}
